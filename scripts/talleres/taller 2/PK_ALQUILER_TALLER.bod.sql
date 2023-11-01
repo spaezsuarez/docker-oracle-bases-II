f8cd517f8f25c67fdbe6258045091cfbfa249298
sergio-paez <sergio.paez@puntored.co>
2023-10-31 20:25:50 -0500

    fix headers and a part of the body package

--- scripts/talleres/taller 2/PK_ALQUILER_TALLER.bod.sql
+++ scripts/talleres/taller 2/PK_ALQUILER_TALLER.bod.sql
@@ -4,7 +4,8 @@ create or replace PACKAGE BODY PK_ALQUILER_TALLER AS
   Descripcion: Paquete que contiene las variables globales, funciones y procedimientos
                asociados al módulo de Reservas
   Autor      : Alba Consuelo Nieto.
---------------------------------------------------------------------------------------*/
+--------------------------------------------------------------------------------------
+    */
      /*------------------------------------------------------------------------------
      Procedimiento para buscar los datos básicos de un cliente
      Parametros de Entrada: pk_codcliente    Código del cliente a buscar
@@ -44,15 +45,15 @@ create or replace PACKAGE BODY PK_ALQUILER_TALLER AS
      Retorna                : Código del cliente 
      Excepcion              : En caso de no encontrar el cliente
     */
-    FUNCTION FU_BUSCAR_CLIENTE(pi_tipoId         IN cliente.i_tipoId%TYPE,
+    FUNCTION FU_BUSCAR_CLIENTE(pi_tipoid         IN cliente.i_tipoid%TYPE,
                                pq_identificacion IN cliente.q_identificacion%TYPE) RETURN NUMBER
     IS
     --Declaración de variables locales
-    lk_codcliente cliente.k_codcliente%TYPE;
+        lk_codcliente cliente.k_codcliente%TYPE;
     BEGIN
         SELECT k_codcliente INTO  lk_codcliente FROM cliente
-        WHERE i_tipoId = pi_tipoId AND q_identificacion = pq_identificacion; 
-        RETURN l_codcliente;
+        WHERE i_tipoId = pi_tipoid AND q_identificacion = pq_identificacion; 
+        RETURN lk_codcliente;
     EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RAISE_APPLICATION_ERROR(-20001, 'Cliente no encontrado');
@@ -70,21 +71,20 @@ create or replace PACKAGE BODY PK_ALQUILER_TALLER AS
                                                0, en caso contrario
                               pm_error         Mensaje de error si hay error o null en caso contrario
    */
-   PROCEDURE PR_LISTAR_RESERVAS(pi_tipoId         IN cliente.i_tipoid%TYPE,
+    PROCEDURE PR_LISTAR_RESERVAS(pi_tipoid        IN cliente.i_tipoid%TYPE,
                                 pq_identificacion IN cliente.q_identificacion%TYPE, 
                                 pc_error          OUT NUMBER, 
                                 pm_error          OUT VARCHAR)
     IS
-        l_cliente CLIENTE%ROWTYPE;
-        --Cursores
         CURSOR c_reservas(ck_cliente CLIENTE.K_CODCLIENTE%TYPE) IS SELECT K_RESERVA,F_INICIAL,F_FINAL,V_TOTAL,I_ESTADO FROM RESERVA WHERE K_CODCLIENTE = ck_cliente;
         CURSOR c_vehiculos(ck_reserva RESERVA.K_RESERVA%TYPE) IS SELECT v.k_placa,vr.f_entrega,vr.f_devolucion,vr.v_alquiler FROM VEHICULO v INNER JOIN VEHICULO_RESERVA vr ON V.K_PLACA = VR.K_PLACA WHERE VR.K_RESERVA = ck_reserva;
         lc_reservas c_reservas%ROWTYPE;
         lc_vehiculos c_vehiculos%ROWTYPE;
+        l_cliente CLIENTE%ROWTYPE;
     BEGIN
         pc_error := 0;
         pm_error := '';
-        select * INTO l_cliente from cliente cl where cl.i_tipoid = pi_tipoId and cl.q_identificacion = pq_identificacion;
+        select * INTO l_cliente from cliente cl where cl.i_tipoid = pi_tipoid and cl.q_identificacion = pq_identificacion;
         IF l_cliente IS NULL THEN
             RAISE NO_DATA_FOUND;
         END IF;
@@ -92,7 +92,6 @@ create or replace PACKAGE BODY PK_ALQUILER_TALLER AS
         DBMS_OUTPUT.PUT_LINE('Identificación Cliente: '|| l_cliente.q_identificacion);
         DBMS_OUTPUT.PUT_LINE('Nombre Cliente: '|| l_cliente.N_NOMBRE1 || ' ' || l_cliente.N_NOMBRE2);
         DBMS_OUTPUT.PUT_LINE('Apellido Cliente: '|| l_cliente.N_APELLIDO1 || ' ' || l_cliente.N_APELLIDO2);
-        DBMS_OUTPUT.PUT_LINE('N Reserva'||'    F Reserva'||'    Vlor total');
         FOR lc_reservas IN c_reservas(l_cliente.k_codcliente) LOOP
             DBMS_OUTPUT.PUT_LINE('RESERVA: '||lc_reservas.K_RESERVA||' '||lc_reservas.F_INICIAL||' '||lc_reservas.F_FINAL||' '||lc_reservas.V_TOTAL||' '||lc_reservas.I_ESTADO);
             FOR lc_vehiculos IN c_vehiculos(lc_reservas.K_RESERVA) LOOP
@@ -115,22 +114,22 @@ create or replace PACKAGE BODY PK_ALQUILER_TALLER AS
      Retorna                : Booleano para indicar la disponibilidad del vehiculo 
      Excepcion              : En caso de no encontrar el vehiculo
    */
-   FUNCTION FU_VEHICULO_DISPONIBLE(pk_placa IN VEHICULO.K_PLACA%TYPE,
-                                   pf_fecha IN  RESERVA.F_RESERVA%TYPE)  RETURN BOOLEAN;         
-    IS
-    --Declaración de variables locales
-    l_vehiculo VEHICULO%ROWTYPE;
-    lf_fecha_reserva RESERVA.F_RESERVA%TYPE;
+    FUNCTION FU_VEHICULO_DISPONIBLE(pk_placa IN VEHICULO.K_PLACA%TYPE,
+                                   pf_fecha IN  RESERVA.F_RESERVA%TYPE) RETURN BOOLEAN
+    AS
+        l_vehiculo VEHICULO%ROWTYPE;
+        lf_fecha_reserva RESERVA.F_RESERVA%TYPE := NULL;
     BEGIN
-        SELECT * INTO  l_vehiculo FROM VEHICULO WHERE K_PLACA = pk_placa;
-        IF l_vehiculo IS NULL THEN
-            RAISE NO_DATA_FOUND;
-        END IF;
+        SELECT V.* INTO  l_vehiculo FROM VEHICULO V WHERE V.K_PLACA = pk_placa;
         SELECT r.f_reserva into lf_fecha_reserva FROM vehiculo_reserva vr
         INNER JOIN reserva r on vr.k_reserva = r.k_reserva  
         where vr.k_placa = pk_placa
         and r.f_reserva = pf_fecha;
-        return lf_fecha_reserva is null;
+        IF lf_fecha_reserva = NULL THEN
+            RETURN TRUE;
+        ELSE
+            RETURN FALSE;
+        END IF;
     EXCEPTION
     WHEN NO_DATA_FOUND THEN
         RAISE_APPLICATION_ERROR(-20001, 'Vehiculo no encontrado');
--- scripts/talleres/taller 2/PK_ALQUILER_TALLER.hed.sql
+++ scripts/talleres/taller 2/PK_ALQUILER_TALLER.hed.sql
@@ -12,15 +12,15 @@ CREATE OR REPLACE PACKAGE PK_ALQUILER_TALLER AS
      q_identificacion   cliente.q_identificacion%TYPE,
      n_nombre1          cliente.n_nombre1%TYPE,
      n_nombre2          cliente.n_nombre2%TYPE,
-     n_apellido1        cliente.n_apellido1,
+     n_apellido1        cliente.n_apellido1%TYPE,
      n_apellido2        cliente.n_apellido2%TYPE
      );
      -- Variable global de tipo registro cliente
      gr_cliente gtr_cliente;
      --Variables globales: Estado de la reserva
-     gv_cancelada   CONSTANT CHAR(1)  = 'C'; 
-     gv_activa      CONSTANT CHAR(1)  = 'A';
-     gv_pagada      CONSTANT CHAR(1)  =  P;
+     gv_cancelada  CHAR  := 'C'; 
+     gv_activa     CHAR  := 'A';
+     gv_pagada     CHAR  :=  'P';
 
      /*------------------------------------------------------------------------------
      Procedimiento para buscar los datos básicos de un cliente
@@ -33,7 +33,7 @@ CREATE OR REPLACE PACKAGE PK_ALQUILER_TALLER AS
     PROCEDURE PR_BUSCAR_CLIENTE(pk_codcliente   IN cliente.k_codcliente%TYPE,
                                 pr_cliente      OUT gtr_cliente,
                                 pc_error        OUT NUMBER,
-                                pm_error        OUT VARCHAR(50));
+                                pm_error        OUT VARCHAR);
 
      /*------------------------------------------------------------------------------
      Función para retornar el código de un cliente
@@ -43,7 +43,7 @@ CREATE OR REPLACE PACKAGE PK_ALQUILER_TALLER AS
      Excepcion              : En caso de no encontrar el cliente
     */
     FUNCTION FU_BUSCAR_CLIENTE(pi_tipoId         IN cliente.i_tipoId%TYPE,
-                               pq_identificacion IN cliente.q_identificacion%TYPE) RETURN NUMBER(12);
+                               pq_identificacion IN cliente.q_identificacion%TYPE) RETURN NUMBER;
 
     /*-----------------------------------------------------------------------------------------
      Procedimiento para listar las reservas de un cliente.
@@ -66,7 +66,7 @@ CREATE OR REPLACE PACKAGE PK_ALQUILER_TALLER AS
      
      Parametros de Entrada  : pk_placa         placa del vehiculo
                               pf_fecha         Fecha en la cual se valida la disponibilidad del vehiculo
-     Retorna                : Booleano para indicar la disponibilidad del vehiculo 
+     Retorna                : Booleano para indicar la disponibilidad del vehiculo
      Excepcion              : En caso de no encontrar el vehiculo
    */
    FUNCTION FU_VEHICULO_DISPONIBLE(pk_placa IN  VEHICULO.K_PLACA%TYPE,
